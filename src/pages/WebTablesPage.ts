2ad3366948b9ff4769ea84625a8ea2524dbee6cc
Soudip <dassoudip97@gmail.com>
2025-05-08 18:39:10 +0530

    Modified WebPageTablesPage to add constructor and instances variables for locator initialization.

--- src/pages/WebTablesPage.ts
+++ src/pages/WebTablesPage.ts
@@ -1,6 +1,12 @@
 import {test,expect,Locator,Page} from '@playwright/test';
 
 export class WebTablesPage{
-    
+    private page:Page;
+    private webTablesHeader: Locator;
+
+    constructor(page:Page){
+        this.page = page;
+        this.webTablesHeader = page.getByRole('heading', {name:'Web Tables'});
+    }
 }
 
