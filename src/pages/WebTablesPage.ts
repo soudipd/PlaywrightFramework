9658566a6958dc830270846baa048af6bea73c97
Soudip <dassoudip97@gmail.com>
2025-05-10 15:17:35 +0530

    refactor: added get method for column headers other than firstname to use it in test for assertions.

--- src/pages/WebTablesPage.ts
+++ src/pages/WebTablesPage.ts
@@ -5,12 +5,24 @@ export class WebTablesPage{
     private webTablesHeader: Locator;
     private rowsCount: Locator;
     private firstNameHeader: Locator;
+    private lastNameHeader: Locator;
+    private emailHeader: Locator;
+    private ageHeader: Locator;
+    private salaryHeader: Locator;
+    private departmentHeader: Locator;
+    private actionHeader: Locator;
 
     constructor(page:Page){
         this.page = page;
         this.webTablesHeader = page.getByRole('heading', {name:'Web Tables'});
         this.rowsCount = page.getByRole('rowgroup');
         this.firstNameHeader= page.getByRole('columnheader', {name:'First Name'});
+        this.lastNameHeader= page.getByRole('columnheader', {name:'Last Name'});
+        this.emailHeader= page.getByRole('columnheader', {name:'Email'});
+        this.ageHeader= page.getByRole('columnheader', {name:'Age'});
+        this.salaryHeader= page.getByRole('columnheader', {name:'Salary'});
+        this.departmentHeader= page.getByRole('columnheader', {name:'Department'});
+        this.actionHeader= page.getByRole('columnheader', {name:'Actions'});
     }
     async waitForPageToLoad(){
         await this.page.waitForSelector('text=Web Tables');
@@ -30,8 +42,28 @@ export class WebTablesPage{
     async getFirstNameHeaderText(){
         return this.firstNameHeader.textContent();
     }
+    async getLastNameHeaderText(){
+        return this.lastNameHeader.textContent();
+    }
+    async getEmailHeaderText(){
+        return this.emailHeader.textContent();
+    }
+    async getAgeHeaderText(){
+        return this.ageHeader.textContent();
+    }
+    async getSalaryHeaderText(){
+        return this.salaryHeader.textContent();
+    }
+    async getDepartmentHeaderText(){
+        return this.departmentHeader.textContent();
+    }
+    async getActionHeaderText(){
+        return this.actionHeader.textContent();
+    }
     async getFirstNameHeader(){
         return this.firstNameHeader;
     }
+
+
 }
 
